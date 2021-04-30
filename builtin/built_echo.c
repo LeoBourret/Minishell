709fb14f36a15c9d048625ed6926a5ca05a92cda
Leo BOURRET <lebourre@e2r2p16.42.fr>
2021-04-30 17:13:12 +0200

    Hello

--- builtin/built_echo.c
+++ builtin/built_echo.c
@@ -1,11 +1,27 @@
 #include "../includes/minishell.h"
 
-int builtin_echo(char ***cmds)
+int		is_n(char *s)
 {
 	int i;
 
 	i = 0;
-	if (ft_strcmp(cmds[0][1], "-n") == 0)
+	if (s[i] == '-')
+		i++;
+	else
+		return (0);
+	while (s[i] == 'n')
+		i++;
+	if (s[i] == '\0')
+		return (1);
+	return (0);
+}
+
+int		builtin_echo(char ***cmds)
+{
+	int i;
+
+	i = 0;
+	if ((is_n(cmds[0][1])) == 1)
 	{
 		i++;
 		while (cmds[0][++i])
@@ -14,10 +30,10 @@ int builtin_echo(char ***cmds)
 		fflush(stdout);
 	}
 	else
-	{	
+	{
 		while (cmds[0][++i])
 			printf("%s ", cmds[0][i]);
 		printf("\n");
 	}
-return 0;
+	return (0);
 }
--- builtin/built_export.c
+++ builtin/built_export.c
@@ -6,56 +6,61 @@
 /*   By: jurichar <jurichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 13:50:56 by lebourre          #+#    #+#             */
-/*   Updated: 2021/04/30 10:01:59 by jurichar         ###   ########.fr       */
+/*   Updated: 2021/04/30 16:29:37 by lebourre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+int		env_swap(t_env_lst **curr, t_env_lst **prev, t_env_lst **begin, int i)
+{
+	t_env_lst	*swap;
+	t_env_lst	*tmp_next;
+
+	swap = *curr;
+	tmp_next = swap->next->next;
+	*curr = (*curr)->next;
+	(*curr)->next = swap;
+	(*curr)->next->next = tmp_next;
+	if (i == 0)
+		*begin = *curr;
+	else
+		(*prev)->next = *curr;
+	*curr = *begin;
+	return (0);
+}
+
 t_env_lst        *env_sort(t_env_lst *list, int i)
 {
-    t_env_lst    *curr;
-    t_env_lst    *begin;
-    t_env_lst    *swap;
-    t_env_lst    *tmp_next;
-    t_env_lst    *prev;
+	t_env_lst	*curr;
+	t_env_lst	*begin;
+	t_env_lst	*prev;
 
-    curr = list;
-    begin = list;
-    prev = list;
-    while (curr->next)
-    {
-        if ((ft_strcmp(curr->name, curr->next->name)) > 0)
-        {
-            swap = curr;
-            tmp_next = swap->next->next;
-            curr = curr->next;
-            curr->next = swap;
-            curr->next->next = tmp_next;
-            if (i == 0)
-                begin = curr;
-            else
-                prev->next = curr;
-            curr = begin;
-            i = 0;
-        }
-        else
-        {
-            prev = curr;
-            curr = curr->next;
-            i++;
-        }
-    }
-    return (begin);
+	curr = list;
+	begin = list;
+	prev = list;
+	i = 0;
+	while (curr->next)
+	{
+		if ((ft_strcmp(curr->name, curr->next->name)) > 0)
+			i = env_swap(&curr, &prev, &begin, i);
+		else
+		{
+			prev = curr;
+			curr = curr->next;
+			i++;
+		}
+	}
+	return (begin);
 }
 
 int builtin_export(char ***cmds, t_env_lst *envlst)
 {
-    envlst = env_sort(envlst, 0);
-    while (envlst)
-    {
-        printf("%s=%s\n", envlst->name, envlst->content);
-        envlst = envlst->next;
-    }
-    return 0;
+	envlst = env_sort(envlst, 0);
+	while (envlst)
+	{
+		printf("%s=%s\n", envlst->name, envlst->content);
+		envlst = envlst->next;
+	}
+	return 0;
 }
